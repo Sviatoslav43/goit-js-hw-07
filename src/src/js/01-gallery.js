1d2f3ae43968743c3bae24968971f735441822f6
Sviatoslav43 <navolskiy17@gmail.com>
2022-10-08 13:28:33 +0300

    task1 done

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('div.gallery')
+
+console.log(galleryRef)
+
+
+function createGalleryItemMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML("beforeend", createGalleryItemMarkup(galleryItems))
+
+// const photoAddToGallery = document.addEventListener('click',)
+function onGallery(event) {
+    event.preventDefault()
+
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+
+    const modalWindow = basicLightbox.create( 
+            `<img
+              class="gallery__image"
+              src="${event.target.dataset.source}"
+              alt="${event.target.value}"
+            />`)
+    modalWindow.show()
+    
+}
+
+galleryRef.addEventListener('click', onGallery)
+
+
--- src/src/js/02-lightbox.js
+++ src/src/js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+
+console.log(galleryRef)
+
+
+function createGalleryItemMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+      `;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML("beforeend", createGalleryItemMarkup(galleryItems))
+
